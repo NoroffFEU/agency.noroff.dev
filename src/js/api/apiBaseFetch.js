8752764348b2505f61f9dfb62063f8c1cc9f4a77
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-01-11 10:21:59 +0100

    adding some information regarding how to use the fetch function

--- src/js/api/apiBaseFetch.js
+++ src/js/api/apiBaseFetch.js
@@ -25,3 +25,16 @@ export async function apiBaseFetch(url, headers = null, offset = 0, limit = 100)
       throw error;
     }
   }
+
+
+
+  // HOW TO USE THIS FUNCTION 
+
+  // You can use the function by providing the url of the API endpoint, custom headers in a dictionary form and offset and limit as integers for pagination.
+
+const headers1 = {'Authorization': 'Token YOUR_API_KEY'};
+const data1 = await fetch('https://api.example.com/items', headers, 0, 25);
+
+// You can also use this function with an argument, like offset and limit as default. and then call it without those argument.
+const headers2 = {'Authorization': 'Token YOUR_API_KEY'};
+const data2 = await fetch('https://api.example.com/items', headers);
