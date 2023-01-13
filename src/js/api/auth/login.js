bcd68a007b069fd9d95dc296212fa6e4e8e6e346
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-01-13 22:02:49 +0100

    Created preliminary login function

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,66 @@
-export async function login(profile) {}
+import { Store } from '../../storage/storage.js';
+
+// Temporary
+const action = '';
+const method = 'POST';
+
+/**
+ * Function for logging in an existing user in database by storing the returned token in localstorage
+ * @param {object} profile Takes in input values from loginForm
+ */
+export async function login(profile) {
+  /* 
+  Responses Parameters
+    Code: 200
+    Content type: application/json
+  */
+
+  // Temporary
+  const loginURL = '';
+  const body = JSON.stringify(profile);
+  const options = {
+    method,
+    body,
+    headers: {
+      'Content-Type': 'application/json;',
+    },
+  };
+
+  try {
+    /* data: {
+    userId: String,
+    firstName: String,
+    lastName: String,
+    email: email,
+    token: string,
+    } */
+
+    const response = await fetch(loginURL, options);
+    const { token, ...profile } = await response.json();
+
+    switch (response.status) {
+      // Let me know if there are any particular server responses I should add and check for.
+
+      case 200: // Successful request
+        Store('token', token);
+        if (profile.admin) {
+          location.replace('#');
+        } else if (profile.company) {
+          location.replace('#');
+        } else {
+          location.replace('#');
+        }
+        break;
+      case 401:
+        // Temporary error handling
+        alert('Wrong username/passowrd');
+        break;
+      default:
+        // Temporary error handling
+        throw new Error();
+    }
+  } catch {
+    // Temporary error handling
+    console.log(error);
+  }
+}
