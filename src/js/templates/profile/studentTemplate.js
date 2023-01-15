5712f4eba9350079482db12586e9e60daa24a7e0
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-01-15 17:30:07 +0100

    Created setup function for student render functions

---
+++ src/js/templates/profile/studentTemplate.js
@@ -0,0 +1,12 @@
+/**
+ * Setup function for student render functions
+ * @param {*} data object data from student model
+ */
+export function studentTemplate(data) {
+  const studentOffers = document.querySelector('#studentOffers');
+  const studentApplications = document.querySelector('#studentApplications');
+  const studentFavoriteListings = document.querySelector('#studentFavoriteListings');
+
+  // Missing renderListing function
+  // Missing lo-fi view of applications sent
+}
