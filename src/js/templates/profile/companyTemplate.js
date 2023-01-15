aaee86054b1d3b2bf8d8417cdb25831f5b763a7e
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-01-15 16:42:25 +0100

    Created renderCompanyApplications() with preliminary work for render

--- src/js/templates/profile/companyTemplate.js
+++ src/js/templates/profile/companyTemplate.js
@@ -3,10 +3,10 @@ export function companyTemplate(data) {}
 export function renderCompanyListings(data) {
   const { listing } = data;
 
-  const listings = document.createElement('div');
+  const listingsContainer = document.createElement('div');
 
-  listing.forEach((object) => {
-    // const { } = object;
+  listing.forEach((listObject) => {
+    // const { } = listObject;
     const listing = document.createElement('div');
     const image = document.createElement('img');
 
@@ -28,10 +28,40 @@ export function renderCompanyListings(data) {
     details.append(applicants, expires, viewButton);
     body.append(title, description, details);
     listing.append(image, body);
-    listings.append(listing);
+    listingsContainer.append(listing);
 
     return listing;
   });
 
-  return listings;
+  return listingsContainer;
 }
+
+export function renderCompanyApplications(data) {
+  const { applications } = data;
+
+  const applicationsContainer = document.createElement('div');
+
+  applications.forEach((appObject) => {
+    // const {} = appObject;
+    const applicantDetails = document.createElement('div');
+    const position = document.createElement('div');
+    const candidate = document.createElement('div');
+    const appliedDate = document.createElement('div');
+    const viewButton = document.createElement('div');
+
+    applicantDetails.classList.add('row');
+    position.classList.add('col');
+    candidate.classList.add('col');
+    appliedDate.classList.add('col');
+    viewButton.classList.add('btn col');
+
+    applicantDetails.append(position, candidate, appliedDate, viewButton);
+    applicationsContainer.append(applicantDetails);
+
+    return applicantDetails;
+  });
+
+  return applicationsContainer;
+}
+
+// Missing lo-fi of offers sent.
