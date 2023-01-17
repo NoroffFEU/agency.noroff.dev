509b902b6a934dbc050b1a231f7312e626182d04
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-01-17 11:06:37 +0100

    navigation changes

--- src/js/ui/header/index.js
+++ src/js/ui/header/index.js
@@ -73,9 +73,6 @@ export const checkState = () => {
 
     return (navBarNav.innerHTML = `<ul class="navbar-nav gap-2">
     <li class="nav-item">
-      <a class="nav-link text-white" aria-current="page" href="#">Profile</a>
-    </li>
-    <li class="nav-item">
       <a class="nav-link text-white" href="#">Listings</a>
     </li>
     <li class="nav-item">
