5836eea76a0401a720a6ef561850de35f68347f8
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-01-17 18:10:48 +0100

    Set up files for custom scss _profileCustomClasses _profileCustomVariables and index.scss

--- src/js/templates/profile/userDetalisTemplate.js
+++ src/js/templates/profile/userDetalisTemplate.js
@@ -27,42 +27,74 @@ export function renderUserDetails(data, parent) {
   const editButton = document.createElement('button');
 
   // Classes
-  image.classList.add('rounded-circle', 'd-block', 'mx-auto');
-  fullName.classList.add('text-center');
-  role.classList.add('text-center');
-  body.classList.add('p-2', 'bg-theme-light', 'my-2');
-  editButton.classList.add('btn', 'btn-dark');
+  image.classList.add('rounded-circle', 'd-block', 'mx-auto', 'w-25');
+  fullName.classList.add('text-center', 'mt-3');
+  role.classList.add('text-center', 'fs-2', 'fw-lighter');
+  body.classList.add('px-4', 'pt-3', 'pb-2', 'bg-theme-light', 'my-2', 'shadow', 'mx-auto', 'rounded-4', 'my-3', 'maxWidthContainer');
+  header.classList.add('fs-5');
+  editButton.classList.add('btn', 'btn-dark', 'd-block', 'mx-auto');
+
+  // Dummy Data
+  image.src = 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80';
+
+  fullName.innerHTML = 'Peter Parker';
+  role.innerHTML = 'Front-End Developer';
+
+  const skillContainer = document.createElement('div');
+  const skillWrapper = document.createElement('div');
+  const skillHeader = document.createElement('h2');
+  skillHeader.innerHTML = 'Skills';
+
+  skillContainer.append(skillHeader, skillWrapper);
+  skillHeader.classList.add('fs-5');
+  skillContainer.classList.add('px-4', 'py-3', 'bg-theme-light', 'my-2', 'shadow', 'mx-auto', 'rounded-4', 'my-3', 'maxWidthContainer');
+  skillWrapper.classList.add('d-flex', 'gap-3', 'flex-wrap');
+
+  const skills = ['HTML', 'CSS', 'JavaScript', 'SCSS', 'Bootstrap', 'React'];
+  skills.forEach((skill) => {
+    const renderSkill = document.createElement('span');
+
+    renderSkill.classList.add('fw-bolder', 'fs-6');
+    renderSkill.innerHTML = skill;
+
+    skillWrapper.append(renderSkill);
+  });
+
+  header.innerHTML = 'About Me';
+  about.innerHTML = `Hello Everyone! Just your friendly neighbourhood WEB developer! Get it?
+
+  I specialize in solving and fixing WCAG crimes, but I do not fight bugs, whatsoever! Bugs are friends that tell us what's wrong with our code, which is very important for learning.`;
 
   // Some conditional logic needs to be applied
-  if (company) {
-    const { name, logo, admin } = data;
+  // if (company) {
+  //   const { name, logo, admin } = data;
 
-    image.src = logo;
-    image.alt = `${name}'s logo`;
+  //   image.src = logo;
+  //   image.alt = `${name}'s logo`;
 
-    fullName.innerHTML = name;
-    role.innerHTML = admin; //admin? Also an array. Placeholder
+  //   fullName.innerHTML = name;
+  //   role.innerHTML = admin; //admin? Also an array. Placeholder
 
-    header.innerHTML = 'About Us';
-    about.innerHTML = 'Lorem Ipsum'; // Placeholder
-  } else {
-    const { firstName, lastName, avatar, role } = data;
+  //   header.innerHTML = 'About Us';
+  //   about.innerHTML = 'Lorem Ipsum'; // Placeholder
+  // } else {
+  //   const { firstName, lastName, avatar, role } = data;
 
-    image.src = avatar;
-    image.alt = `${firstName} ${lastName}'s avatar`;
+  //   image.src = avatar;
+  //   image.alt = `${firstName} ${lastName}'s avatar`;
 
-    fullName.innerHTML = firstName + lastName;
-    role.innerHTML = role;
+  //   fullName.innerHTML = firstName + lastName;
+  //   role.innerHTML = role;
 
-    header.innerHTML = 'About Me';
-    about.innerHTML = 'Lorem Ipsum'; // Placeholder
-  }
+  //   header.innerHTML = 'About Me';
+  //   about.innerHTML = 'Lorem Ipsum'; // Placeholder
+  // }
 
   editButton.innerHTML = 'Edit Profile';
 
   // Appending
   body.append(header, about);
-  userDetailsContainer.append(image, fullName, role, body, editButton);
+  userDetailsContainer.append(image, fullName, role, skillContainer, body, editButton);
   parent.append(userDetailsContainer);
 
   return userDetailsContainer;
