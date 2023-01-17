886d84ad57e802a75022a352f845426f0a361d82
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2023-01-17 20:43:56 +0100

    Correcting of merge issue with index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 import { header } from './src/js/ui/header/index.js';
 header();
 
 import { footer } from './src/js/ui/footer/index.js';
 footer();
-=======
-import * as ui from './src/js/ui/index.js'
+import * as ui from './src/js/ui/index.js';
 
-ui.displayBaseLayout()
->>>>>>> FE-offers
+ui.displayBaseLayout();
