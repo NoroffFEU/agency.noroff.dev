4f718c000db650fe7f94ea68ea4e18a06ee18044
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2023-01-18 11:27:16 +0100

    added register user endpoint and reference link

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -4,9 +4,10 @@ export const apiPath = apiUrl.toString()
 
 /* This section is for Dummy API that is for testing purpose and development
    it`s a well documented dummy API so make sure to read the docs for it.
-   All endpoints below are dummy endpoints */
-
-export const dummyApiLogin = "https://dummyjson.com/auth/login"; // Dummy Login
+   All endpoints below are dummy endpoints
+   Docs for dummy API can be found here https://dummyjson.com/docs */
+export const dummyApiRegisterUser = "https://dummyjson.com/users/add"// It will simulate a POST request and will return the new created user with a new id
+export const dummyApiLogin = "https://dummyjson.com/auth/login"; // user Login
 export const dummyApiGetAll = "https://dummyjson.com/posts"; // GET all posts
 export const dummyApiGetSingel = "https://dummyjson.com/posts/1"//GET a singel post
 export const dummyApiSearch = "https://dummyjson.com/posts/search?q=love";// Search for a word in post
@@ -17,3 +18,4 @@ export const dummyApiGetPostByID = "https://dummyjson.com/posts/user/"//This nee
 
 // Limit resources to be viewed 
 export const dummyApiLimitResources = "https://dummyjson.com/RESOURCE/?limit=10&skip=5&select=key1,key2,key3";//Url with limit
+
