b9ae8ad822088573c578c6b758da641a7be15b4a
Christopher <46559129+Christonn93@users.noreply.github.com>
2023-01-18 11:32:21 +0100

    Update constants.js
    just some changes, dont like it?

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -5,17 +5,22 @@ export const apiPath = apiUrl.toString()
 /* This section is for Dummy API that is for testing purpose and development
    it`s a well documented dummy API so make sure to read the docs for it.
    All endpoints below are dummy endpoints
-   Docs for dummy API can be found here https://dummyjson.com/docs */
-export const dummyApiRegisterUser = "https://dummyjson.com/users/add"// It will simulate a POST request and will return the new created user with a new id
-export const dummyApiLogin = "https://dummyjson.com/auth/login"; // user Login
-export const dummyApiGetAll = "https://dummyjson.com/posts"; // GET all posts
-export const dummyApiGetSingel = "https://dummyjson.com/posts/1"//GET a singel post
-export const dummyApiSearch = "https://dummyjson.com/posts/search?q=love";// Search for a word in post
-export const dummyApiCreatePost = "https://dummyjson.com/posts/add";//create a post
-export const dummyAPiUpddatePost = "https://dummyjson.com/posts/1"; //Update a post
-export const dummyApiDeletePost = "https://dummyjson.com/posts/1";//Delete a post
-export const dummyApiGetPostByID = "https://dummyjson.com/posts/user/"//This needs a id of a user from query
+   Docs for dummy API can be found here https://dummyjson.com/docs 
+*/
+
+
+export const dummyApiUrl = "https://dummyjson.com/" // Base url
+
+export const dummyApiRegisterUser = "users/add" // It will simulate a POST request and will return the new created user with a new id
+export const dummyApiLogin = "auth/login"; // user Login
+export const dummyApiGetAll = "posts"; // GET all posts
+export const dummyApiGetSingel = "posts/1" //GET a singel post
+export const dummyApiSearch = "posts/search?q=love"; // Search for a word in post
+export const dummyApiCreatePost = "posts/add"; //create a post
+export const dummyAPiUpddatePost = "posts/1"; //Update a post
+export const dummyApiDeletePost = "posts/1"; //Delete a post
+export const dummyApiGetPostByID = "posts/user/" //This needs a id of a user from query
 
 // Limit resources to be viewed 
-export const dummyApiLimitResources = "https://dummyjson.com/RESOURCE/?limit=10&skip=5&select=key1,key2,key3";//Url with limit
+export const dummyApiLimitResources = "RESOURCE/?limit=10&skip=5&select=key1,key2,key3"; //Url with limit
 
