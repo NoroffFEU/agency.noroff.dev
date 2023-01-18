8c86537a0f5b9d7ec2acaf6518a88444c902fcbd
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-01-18 19:32:03 +0100

    changes to styling and assets

--- src/js/templates/admin/users/index.js
+++ src/js/templates/admin/users/index.js
@@ -4,38 +4,41 @@
 
 export function userTemplate(userData) {
     const profile = document.createElement('div');
-    profile.classList.add('row', 'px-5');
+    profile.classList.add('row', 'ps-2', 'px-md-5');
 
     const nameContainer = document.createElement('p');
-    nameContainer.classList.add('col-5', 'bg-light', 'px-2', 'py-3', 'mb-2');
+    nameContainer.classList.add('col-5', 'bg-light', 'px-2', 'py-3', 'mb-2', 'overflow-hidden');
     const name = userData.fullName;
     nameContainer.append(name);
     profile.appendChild(nameContainer);
 
     const emailContainer = document.createElement('p');
-    emailContainer.classList.add('col-5', 'bg-light', 'px-2', 'py-3', 'mb-2');
+    emailContainer.classList.add('col-5', 'bg-light', 'px-2', 'py-3', 'mb-2', 'overflow-hidden');
     const email = userData.email;
     emailContainer.append(email);
     profile.appendChild(emailContainer);
 
     const activeContainer = document.createElement('div');
-    activeContainer.classList.add('col-1', 'd-none', 'd-lg-block', 'text-center', 'py-3',);
-    const active = document.createElement('i');
+    activeContainer.classList.add('col-1', 'd-none', 'd-lg-block', 'text-center', 'py-3', 'pb-4');
+    const active = document.createElement('img');
     if (userData.isActive === true) {
-        active.classList.add('fa-solid', 'fa-check');
+        active.src = '/src/assets/icons/checkmark.svg';
+        active.classList.add('activeIcon');
         activeContainer.appendChild(active);
     } else {
-        active.classList.add('fa-solid', 'fa-xmark');
+        active.src = '/src/assets/icons/cancel.svg';
+        active.classList.add('inActiveIcon');
         activeContainer.appendChild(active);
     }
     profile.appendChild(activeContainer);
 
-    const deleteProfileContainer = document.createElement('div');
-    deleteProfileContainer.classList.add('col-2', 'col-lg-1', 'text-center', 'py-3',);
-    const deleteProfile = document.createElement('i');
-    deleteProfile.classList.add('fa-solid', 'fa-trash');
-    deleteProfileContainer.appendChild(deleteProfile);
-    profile.appendChild(deleteProfileContainer);
+    const deleteProfileButton = document.createElement('button');
+    deleteProfileButton.classList.add('col-2', 'col-lg-1', 'text-center', 'my-3', 'mb-4', 'border-0', 'bg-white');
+    const deleteProfileIcon = document.createElement('img');
+    deleteProfileIcon.src = '/src/assets/icons/delete-black.svg';
+    deleteProfileIcon.classList.add('deleteIcon');
+    deleteProfileButton.appendChild(deleteProfileIcon);
+    profile.appendChild(deleteProfileButton);
 
     return profile;
 }
