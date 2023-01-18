8fa125f5add2faffa5c8f842a97716ef1cfc7598
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-01-18 13:20:10 +0100

    Replaced hard coded HTML with dynamic JS templated

---
+++ src/js/templates/listings/options.js
@@ -0,0 +1,23 @@
+export function options() {
+  /*const buttonContainer = document.querySelector('.buttonContainer');*/
+  const cardBody = document.querySelector('.card-body');
+
+  // Container for edit and delete button
+  const options = document.createElement('div');
+  options.className = 'd-flex gap-2';
+
+  // Edit button
+  const editBtn = document.createElement('button');
+  editBtn.className = 'btn btn-theme-secondary text-uppercase rounded-0';
+  editBtn.innerText = 'Edit';
+
+  // Delete button
+  const deleteBtn = document.createElement('button');
+  deleteBtn.className = 'btn btn-theme-dark text-uppercase rounded-0';
+  deleteBtn.innerText = 'Delete';
+
+  options.append(editBtn, deleteBtn);
+  cardBody.append(options);
+
+  /*buttonContainer.classList.add('d-none');*/
+}
---
+++ src/js/templates/listings/singleListingTemplate.js
@@ -0,0 +1,47 @@
+export function singleListingTemplate(listing) {
+  // Content will be updated and altered better when we are provided with correct endpoints, using dummy API for now
+  const column = document.createElement('div');
+  column.className = 'col-lg-6';
+
+  const card = document.createElement('div');
+  card.className = 'card bg-theme-light border-0';
+
+  const img = document.createElement('img');
+  img.className = 'card-img-top';
+  img.src = listing.thumbnail;
+
+  const cardBody = document.createElement('div');
+  cardBody.className = 'card-body d-flex flex-column gap-1 p-sm-3 p-md-5';
+
+  const cardTitle = document.createElement('h1');
+  cardTitle.className = 'card-title';
+  cardTitle.innerText = listing.title;
+
+  const smallText = document.createElement('small');
+  smallText.innerHTML = `${listing.brand} <span>&#x2022;</span> ${listing.category} <span>&#x2022;</span> ${listing.price}`;
+
+  const description = document.createElement('p');
+  description.innerText = listing.description;
+
+  const buttonContainer = document.createElement('div');
+  buttonContainer.className = 'd-flex gap-3 my-3 buttonContainer';
+
+  const applyBtn = document.createElement('a');
+  applyBtn.className = 'btn btn-theme-secondary text-uppercase w-100 rounded-0 applyBtn';
+  applyBtn.innerText = 'Apply Now';
+
+  const favBtn = document.createElement('button');
+  favBtn.className = 'btn btn-theme-light';
+
+  // Will be updated when i have assets
+  const favIcon = document.createElement('i');
+  favIcon.className = 'fa-regular fa-heart';
+
+  column.append(card);
+  card.append(img, cardBody);
+  cardBody.append(cardTitle, smallText, buttonContainer, description);
+  buttonContainer.append(applyBtn, favBtn);
+  favBtn.append(favIcon);
+
+  return column;
+}
