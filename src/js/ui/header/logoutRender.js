ec915bb6876bad391f709687115f55c4994eb07b
saba <89579913+sabaFitwi@users.noreply.github.com>
2023-01-19 22:17:25 +0100

    Update logoutRender.js

--- src/js/ui/header/logoutRender.js
+++ src/js/ui/header/logoutRender.js
@@ -1,21 +1,14 @@
 
-//This should be uncommented when we use the new Store("token")
-//import { Store } from "../../storage/localStorage.mjs";
+import { Store } from "../../storage/localStorage.mjs";
 
 
 const checkLoginStatus = function () {
 
   const loggedIn = document.querySelectorAll("data-visible="loggedIn");
-  const loggedOut = document.querySelectorAll("data-visible="loggedOut");
-                                             
- // The following code should be applied for this scenario 
- // but i couldnot make it work  so i have used the direct  getItem method.
-                                             
+  const loggedOut = document.querySelectorAll("data-visible="loggedOut");                                                                    
 
-       //const store = new Store("token");
-        // const token = store.state
-
-  const token = localStorage.getItem("token");
+       const store = new Store("token");
+        const token = store.state
   
   if (token) {
     loggedOut.forEach((item) => item.classList.add("d-none"));
