c852cc2b07bd0f4a90561bae314a2304c84e1674
saba <89579913+sabaFitwi@users.noreply.github.com>
2023-01-19 15:00:34 +0100

    Update logoutRender.js

--- src/js/ui/header/logoutRender.js
+++ src/js/ui/header/logoutRender.js
@@ -4,9 +4,11 @@ import { isLoginUser } from '../api/auth/loginUser.js'
 
 const checkLoginStatus = function () {
 
-  const loggedIn = document.querySelectorAll(".loginUserButton");
-  const loggedOut = document.querySelectorAll("logoutUserButton");
+  const loggedIn = document.querySelectorAll("data-visible="loggedIn");
+  const loggedOut = document.querySelectorAll("data-visible="loggedOut");
 
+   const store = new Store("token", {});
+  const isLoggedIn = store.state;                                   
 
   if (isLoginUser()) {
     loggedOut.forEach((item) => item.classList.add("d-none"));
