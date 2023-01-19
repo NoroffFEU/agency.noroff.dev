72020f8c932ca5cfef088bdc70099de274728f6b
saba <89579913+sabaFitwi@users.noreply.github.com>
2023-01-19 21:16:22 +0100

    Update render Logout
    This code worked well. But when I get a feedback from the teachers.

--- src/js/ui/header/logoutRender.js
+++ src/js/ui/header/logoutRender.js
@@ -1,17 +1,25 @@
-import { isLoginUser } from '../api/auth/loginUser.js'
 
+//This should be uncommented when we use the new Store("token")
+//import { Store } from "../../storage/localStorage.mjs";
 
 
 const checkLoginStatus = function () {
 
   const loggedIn = document.querySelectorAll("data-visible="loggedIn");
   const loggedOut = document.querySelectorAll("data-visible="loggedOut");
+                                             
+ // The following code should be applied for this scenario 
+ // but i couldnot make it work  so i have used the direct  getItem method.
+                                             
 
-   const store = new Store("token", {});
-  const isLoggedIn = store.state;                                   
+       //const store = new Store("token");
+        // const token = store.state
 
-  if (isLoginUser()) {
+  const token = localStorage.getItem("token");
+  
+  if (token) {
     loggedOut.forEach((item) => item.classList.add("d-none"));
+    
   } else {
 
     loggedIn.forEach((item) => item.classList.add("d-none"));
