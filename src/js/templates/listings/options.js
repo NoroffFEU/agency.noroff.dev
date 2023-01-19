0cddadf5d2920a821405becba1dc7a56c83867e8
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-01-19 18:36:48 +0100

    Adding data attributes + change edit btn to link

--- src/js/templates/listings/options.js
+++ src/js/templates/listings/options.js
@@ -9,14 +9,17 @@ export function options() {
   options.className = 'd-flex gap-2';
 
   // Edit button
-  const editBtn = document.createElement('button');
+  const editBtn = document.createElement('a');
   editBtn.className = 'btn btn-theme-secondary text-uppercase rounded-0';
+  editBtn.dataset.auth = 'editSingleListing';
+  editBtn.href = '/pages/listings/listing/editListing.html';
   editBtn.id = 'editListingBtn';
   editBtn.innerText = 'Edit';
 
   // Delete button
   const deleteBtn = document.createElement('button');
   deleteBtn.className = 'btn btn-theme-dark text-uppercase rounded-0';
+  deleteBtn.dataset.auth = 'deleteListing';
   deleteBtn.id = 'deleteListingBtn';
   deleteBtn.innerText = 'Delete';
 
