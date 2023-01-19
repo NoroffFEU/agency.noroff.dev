bf25b9cd5e083b0a52e4141044c64a1aae59dfa0
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2023-01-19 01:38:01 +0100

    Running "npm run format" this have made corrections of the standard formation for the project.

--- src/js/api/decodeFunction.js
+++ src/js/api/decodeFunction.js
@@ -1,7 +1,7 @@
 /**
- * 
- * @param {*} token Needs token from localstorage 
- * @returns user data that can be used to find user role and other sensitive information. 
+ *
+ * @param {*} token Needs token from localstorage
+ * @returns user data that can be used to find user role and other sensitive information.
  */
 export function decodeFunction(token) {
   const data = decode(token);
