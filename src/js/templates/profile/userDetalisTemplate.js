f6c20ff6be915d99f8969b4b7a0c33baa56deb79
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-01-20 20:07:50 +0100

    Updated profileTemplate for new functions and code format

--- src/js/templates/profile/userDetalisTemplate.js
+++ src/js/templates/profile/userDetalisTemplate.js
@@ -8,101 +8,20 @@ import { Store } from './../../storage/storage.js';
  * @param {object} data object data from user or company model
  */
 export function userDetailsTemplate(data) {
-  const profileContainer = document.querySelector('#profileContainer');
+  const profileImage = document.querySelector('#profileImage');
+  const profileName = document.querySelector('#profileName');
+  const profileRole = document.querySelector('#profileRole');
+  const skillContainer = document.querySelector('#skillsContainer');
+  const skillsList = document.querySelector('#skillsList');
+  const descriptionHeader = document.querySelector('#descriptionHeader');
+  const profileDescription = document.querySelector('#descriptionBody');
 
-  renderUserDetails(data, profileContainer);
-}
-
-/**
- * Function for rendering user details on profile page
- * @param {object} data object data from user model or company model
- * @returns Renders user details on profile page
- */
-export function renderUserDetails(data, parent) {
-  // Elements
-  const userDetailsContainer = document.createElement('div');
-  const image = document.createElement('img');
-  const fullName = document.createElement('h1');
-  const role = document.createElement('p');
-
-  const body = document.createElement('div');
-  const header = document.createElement('h2');
-  const about = document.createElement('p');
-
-  const editButton = document.createElement('button');
-
-  // Classes
-  image.classList.add('rounded-circle', 'd-block', 'mx-auto', 'w-25');
-  fullName.classList.add('text-center', 'mt-3');
-  role.classList.add('text-center', 'fs-2', 'fw-lighter');
-  body.classList.add('px-4', 'pt-3', 'pb-2', 'bg-theme-light', 'my-2', 'shadow', 'mx-auto', 'rounded-4', 'my-3', 'maxWidthProfileContainer');
-  header.classList.add('fs-5');
-  editButton.classList.add('btn', 'btn-dark', 'd-block', 'mx-auto');
-
-  // Dummy Data
-  image.src = 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80';
-
-  fullName.innerHTML = 'Peter Parker';
-  role.innerHTML = 'Front-End Developer';
-
-  const skillContainer = document.createElement('div');
-  const skillWrapper = document.createElement('div');
-  const skillHeader = document.createElement('h2');
-  skillHeader.innerHTML = 'Skills';
-
-  skillContainer.append(skillHeader, skillWrapper);
-  skillHeader.classList.add('fs-5');
-  skillContainer.classList.add('px-4', 'py-3', 'bg-theme-light', 'my-2', 'shadow', 'mx-auto', 'rounded-4', 'my-3', 'maxWidthProfileContainer');
-  skillWrapper.classList.add('d-flex', 'gap-3', 'flex-wrap');
-
-  const skills = ['HTML', 'CSS', 'JavaScript', 'SCSS', 'Bootstrap', 'React'];
-  skills.forEach((skill) => {
-    const renderSkill = document.createElement('span');
-
-    renderSkill.classList.add('fw-bolder', 'fs-6');
-    renderSkill.innerHTML = skill;
-
-    skillWrapper.append(renderSkill);
-  });
-
-  header.innerHTML = 'About Me';
-  about.innerHTML = `Hello Everyone! Just your friendly neighbourhood WEB developer! Get it?
-
-  I specialize in solving and fixing WCAG crimes, but I do not fight bugs, whatsoever! Bugs are friends that tell us what's wrong with our code, which is very important for learning.`;
-
-  // Some conditional logic needs to be applied
-  // if (company) {
-  //   const { name, logo, admin } = data;
-
-  //   image.src = logo;
-  //   image.alt = `${name}'s logo`;
-
-  //   fullName.innerHTML = name;
-  //   role.innerHTML = admin; //admin? Also an array. Placeholder
-
-  //   header.innerHTML = 'About Us';
-  //   about.innerHTML = 'Lorem Ipsum'; // Placeholder
-  // } else {
-  //   const { firstName, lastName, avatar, role } = data;
-
-  //   image.src = avatar;
-  //   image.alt = `${firstName} ${lastName}'s avatar`;
-
-  //   fullName.innerHTML = firstName + lastName;
-  //   role.innerHTML = role;
-
-  //   header.innerHTML = 'About Me';
-  //   about.innerHTML = 'Lorem Ipsum'; // Placeholder
-  // }
-
-  editButton.innerHTML = 'Edit Profile';
-
-  // Appending
-  body.append(header, about);
-  userDetailsContainer.append(image, fullName, role, skillContainer, body, editButton);
-  parent.append(userDetailsContainer);
-
-  return userDetailsContainer;
+  renderProfileImage(data, profileImage);
+  renderProfileName(data, profileName);
+  renderProfileRole(data, profileRole);
+  renderProfileSkills(data, skillsList, skillContainer);
+  renderDescriptionHeader(data, descriptionHeader);
+  renderProfileDescription(data, profileDescription);
 }
 
 /**
@@ -146,17 +65,19 @@ export function renderProfileRole(data, element) {
   return element;
 }
 
-export function renderProfileSkills(data, parent) {
-  if (roleCompany()) return;
+export function renderProfileSkills(data, parent, element) {
+  if (roleCompany()) {
+    element.classList.add('d-none');
+  } else {
+    const { skills } = data;
+    skills.forEach(item, () => {
+      const renderSkill = document.createElement('li');
+      renderSkill.classList.add('fw-bolder', 'fs-6');
+      renderSkill.innerHTML = item;
 
-  const { skills } = data;
-  skills.forEach(item, () => {
-    const renderSkill = document.createElement('li');
-    renderSkill.classList.add('fw-bolder', 'fs-6');
-    renderSkill.innerHTML = item;
-
-    parent.append(renderSkill);
-  });
+      parent.append(renderSkill);
+    });
+  }
 
   return parent;
 }
