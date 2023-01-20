c3baeef948be099e0bbb8c075bc5a8d0ce14339c
Gonzalo Longe <gonlonge@icloud.com>
2023-01-20 10:57:05 +0100

    editListings
    Made some update on editListings

---
+++ src/js/listeners/post/editListings.js
@@ -0,0 +1,22 @@
+// Author: Gonzalo Longe
+
+import { editSingleListing } from '../../api/posts/editSingleListing';
+
+export async function editListingListener() {
+  const form = document.querySelector('#editListings');
+
+  const url = new URL(location.href);
+  const id = url.searchParams.get('id');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const listing = Object.fromEntries(formData.entries());
+    listing.id = id;
+
+    //send it to API
+    editSingleListing(listing);
+    location.reload();
+  });
+}
