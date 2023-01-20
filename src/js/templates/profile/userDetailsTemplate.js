8995213bb27e76159a92d78f0887660f6937b5d5
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-01-20 20:28:42 +0100

    Added comment for future developer(s) in userDetailTemplate()

--- src/js/templates/profile/userDetailsTemplate.js
+++ src/js/templates/profile/userDetailsTemplate.js
@@ -3,6 +3,14 @@ import { Store } from '../../storage/storage.js';
 // Author Truls Haakenstad @Menubrea
 // Dev Team: Frontend - User
 
+/* To Future Developer
+  I've tried to organize content in a matter that I believe should be easy to inherit. Don't expect any of this code to run, because it will certainly not. At the moment I'm writing this, the response object is still not finalized, so variables have been named more so based on familiary for you to understand and guesstimates based on available data and expectations. 
+
+  Conditional logic I would expect to require some tweaking, based on the 3 different views available. As of now there should be at least 3 different views - company(own), user(own) and profile(other). HTML is structured in in a way where there's #studentView and #companyView, both of these are hidden by default with 'd-none'. It's possible it would make more sense to extract the role from the profile response to apply your logic as opposed to a saved key in local storage.
+
+  Any question can be forwarded to Truls H. Haugen in Discord, or @Menubrea on github.
+*/
+
 /**
  * Setup function for profile user details
  * @param {object} data object data from user or company model
