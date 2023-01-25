d29f79db076017df9c39b58b8923b3c475963b69
Menubrea <truls.haakenstad@outlook.com>
2023-01-25 16:34:37 +0100

    Renamed register.mjs to register.js and updated imports

---
+++ src/js/api/auth/register.js
@@ -0,0 +1,20 @@
+import { apiUrl } from '../constants.js';
+
+const action = '/auth/register';
+const method = 'post';
+
+export async function register(profile) {
+  const registerURL = apiUrl + action;
+
+  const response = await fetch(registerURL, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method,
+    body: JSON.stringify(profile),
+  });
+
+  const result = await response.json();
+  location.href = `login.html`;
+  return result;
+}
