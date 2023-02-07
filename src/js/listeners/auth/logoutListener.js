0df7fb320737f704c8a23e881b8a6ed7fcac1128
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-02-07 13:24:14 +0100

    Commented out logout listener as it currently breaks scripts

--- src/js/listeners/auth/logoutListener.js
+++ src/js/listeners/auth/logoutListener.js
@@ -2,4 +2,4 @@ import { logout } from '../../api/auth/index.js';
 
 // The element id  is temporary. need to replace it with the right one.
 
-document.quaryselector('[data-auth=logout]').addEventListener('click', logout);
+// document.quaryselector('[data-auth=logout]').addEventListener('click', logout);
