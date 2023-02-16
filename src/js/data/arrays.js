6cb61b654ddc77360fe5f526b982f35e50d429e0
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-02-16 10:43:09 +0100

    Created folder for data with array.js to hold the array for select options

---
+++ src/js/data/arrays.js
@@ -0,0 +1,65 @@
+/**
+ * Option parameters for degree.
+ */
+export const degreeOptions = [
+  {
+    name: 'Applied Machine Learning',
+  },
+  {
+    name: 'Front-end Development',
+  },
+  {
+    name: 'Back-end Development',
+  },
+  {
+    name: 'Data Analyst',
+  },
+  {
+    name: 'UX/UI-Design',
+  },
+  {
+    name: 'Graphic Design',
+  },
+  {
+    name: 'Music Production',
+  },
+  {
+    name: 'Film Production',
+  },
+  {
+    name: 'Digital Marketing',
+  },
+  {
+    name: 'Video Marketing in Social Media',
+  },
+  {
+    name: '3D Art in Game Technology',
+  },
+  {
+    name: 'Network and IT security',
+  },
+  {
+    name: 'Network and System Administration',
+  },
+  {
+    name: 'Cyber Security',
+  },
+  {
+    name: 'Applied Data Science',
+  },
+  {
+    name: 'Digital Forensics',
+  },
+  {
+    name: 'Interactive Media - Animation',
+  },
+  {
+    name: 'Interaction Media - Games',
+  },
+  {
+    name: 'Modern Project Management',
+  },
+  {
+    name: 'Other',
+  },
+];
