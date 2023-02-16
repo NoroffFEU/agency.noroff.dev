be7e4718f3b96affcc59f0008f576c69167db21d
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-02-16 10:44:18 +0100

    created listeners for handling avatar input and adding a new degree

---
+++ src/js/listeners/profile/addDegreeHandler.js
@@ -0,0 +1,9 @@
+import { createInputSelect } from './../../templates/profile/index.js';
+import { degreeOptions } from '../../data/arrays.js';
+
+export function addDegreeInput() {
+  const button = document.getElementById('degreesButton');
+  const container = document.getElementById('degreesContainer');
+
+  button.addEventListener('click', () => createInputSelect(degreeOptions, container));
+}
