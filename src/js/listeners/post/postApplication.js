6a5ee5a48b4fd052ac5a8fa13300da11e569cb00
ohjenssen <oskar_jenssen@hotmail.com>
2023-03-15 11:48:05 +0100

    Rewrote how the listener retrieves the inputdata from the form

--- src/js/listeners/post/postApplication.js
+++ src/js/listeners/post/postApplication.js
@@ -1,23 +1,24 @@
+// Co-author: Oskar Jenssen
 // It is unable to create anything at the moment, until the proper API is here.
 // import { create } from '../../api/posts/create.js';
 
 export async function postApplication() {
-  const appForm = document.querySelector('#appForm');
+  const applyForm = document.querySelector('#applyForm');
 
-  if (appForm) {
-    appForm.addEventListener('submit', async (event) => {
+  if (applyForm) {
+    applyForm.addEventListener('submit', async (event) => {
       event.preventDefault();
       const appForm = event.target;
       const appFormData = new FormData(appForm);
       const postData = Object.fromEntries(appFormData.entries());
       console.log(postData);
 
-      const appEmail = document.querySelector('#inputEmail');
-      const appAddress = document.querySelector('#inputAddress');
-      const appPhone = document.querySelector('#inputPhone');
-      const appLink = document.querySelector('#inputLink');
-      const appFile = document.querySelector('#inputFile');
-      const appLetter = document.querySelector('#inputLetter');
+      // const appEmail = document.querySelector('#inputEmail');
+      // const appAddress = document.querySelector('#inputAddress');
+      // const appPhone = document.querySelector('#inputPhone');
+      // const appLink = document.querySelector('#inputLink');
+      // const appFile = document.querySelector('#inputFile');
+      // const appLetter = document.querySelector('#inputLetter');
 
       
     //   await create(postData);
