65aa42cb7c73f508d5bc9d12485984aa045eb6ab
Megumi Kimura <kimuramegumi@gmail.com>
2023-03-15 21:46:07 +0100

    deletes TEMP Html and temp routerSwitch

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import { showRegFormListener } from './src/js/listeners/auth/showRegFormListener
 import { renderListings } from './src/js/templates/listings/renderListings.js';
 import { profileRouter } from './src/js/listeners/profile/index.js';
 import { adminRouter } from './src/js/views/admin/adminRouter.js';
-import { setDeleteApplicationListener } from './src/js/api/application/deleteApplication.js';
 
 displayBaseLayout();
 
@@ -89,11 +88,6 @@ const routerSwitch = () => {
       document.querySelector('title').innerText = defaultTitle + ` || ` + '404';
       break;
     default:
-
-    // Template HTML for testing
-    case 'applicationTEMP':
-      setDeleteApplicationListener();
-      break;
   }
 };
 routerSwitch();
