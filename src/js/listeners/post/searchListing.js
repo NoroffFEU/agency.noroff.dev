91c93ca1c0f1b95e462d9e182b73a615f62df322
ohjenssen <oskar_jenssen@hotmail.com>
2023-03-15 11:43:58 +0100

    Function now renders the filtered posts from the searchfunction

--- src/js/listeners/post/searchListing.js
+++ src/js/listeners/post/searchListing.js
@@ -1,22 +1,53 @@
 // Author: Emilie Herrera Thomsen
-// This is just the skeleton for a search functionality, not finished.
+// Co-author: Oskar Jenssen
 
 import { getListOfListings } from "../../api/posts/getListOfListings.js";
-import {renderListings} from "../../templates/listings/renderListings.js";
+import { renderListings } from "../../templates/listings/renderListings.js";
 
-export function searchListings(arrayOfListings) {
-    const search = document.querySelector(".searchInput");
+export async function searchListings(arrayOfListings) {
+    const listings = await getListOfListings();
+    const search = document.querySelector("#searchListing");
+    const listingsContainer = document.querySelector(".listingContainer");
 
-    search.onkeyup = function() {
+    search.onkeyup = function(event) {
         const searchValue = event.target.value.trim().toLowerCase();
-    
-        const filteredListings = arrayOfListings.filter(function(result){
-            if(result.toLocaleLowerCase().startsWith(searchValue)) {
-                return true;
-            };
-        });    
-                
-        renderListings(filteredListings);
+
+        //Filters the listings based on searchvalue
+        const listingsFiltered = listings.products.filter((listing) => {
+            if(listing.title.toLowerCase().match(searchValue)){
+                return true; 
+            } else {
+                return false;
+            }
+        });
+
+        // Empties the listings container and adds the listings matching the searchvalue
+        listingsContainer.innerHTML = "";
+        listingsFiltered.forEach(listing => {
+            listingsContainer.innerHTML += 
+           `
+           <div class="col-sm-6">
+             <div class="card my-3 px-3 bg-theme-light shadow">
+               <div class="col-md-4 mt-5 px-2">
+                 <img src="../../images/logoipsum-287.svg" class="img-fluid rounded-start" alt="..." />
+               </div>
+               <div class="col-md-8">
+                 <div class="card-body">
+                   <h5 class="card-title">${listing.title}</h5>
+                   <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ducimus vitae ipsam tempore! Reiciendis soluta doloremque tempore harum, fugiat laudantium optio eveniet quae deleniti iste.</p>
+                 </div>
+                 <div class="position-absolute bottom-0 end-0 mx-3 my-3">
+                   <div>
+                     <a href="#" class="btn bg-theme-primary">View More</a>
+                   </div>
+               </div>
+               </div>
+             </div>
+           </div>
+           `                   
+        });
+        console.log(listingsFiltered);
+        return listingsFiltered;
     };
 };
 
