596550f4f0b23ebf8b73d90aca043d353eba2010
MariceAshtiani <95356770+MariceAshtiani@users.noreply.github.com>
2023-03-16 19:04:14 +0100

    changed the fetch, method to put, response

--- src/js/api/posts/editSingleListing.js
+++ src/js/api/posts/editSingleListing.js
@@ -1,22 +1,27 @@
 // Author: Gonzalo Longe
 
-import { apiBaseFetch } from '../apiBaseFetch.js';
+import { authBaseFetchOpen } from "../apiBaseFetch";
 /*import { apiUrl } from '../constants.js';*/
 
-export async function editSingleListing(id) {
-  if (!id) {
-    throw new Error('Get requires a listingID');
+const method = "put";
+
+export async function editSingleListing(listingData) {
+  if (!listingData.id) {
+    throw new Error('Update requires a listingID');
   }
 
-  const headers = {
-    'Content-Type': 'application/json',
-  };
+  const url = `https://dummyjson.com/products/${listingData.id}`;
 
-  const url = `https://dummyjson.com/products/${id}`;
 
-  const response = await apiBaseFetch(url, headers);
+  const response = await authBaseFetchOpen(url, {
+    method,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(listingData)
+  });
 
-  if (!response.ok) {
-    return await response;
+  if (response.ok) {
+    return await response.json();
   }
 }
