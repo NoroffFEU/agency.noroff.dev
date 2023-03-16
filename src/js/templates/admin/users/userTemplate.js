d84613cb6341e4e5271442062aab4e1d8927000a
JonasHope <95291501+JonasHope@users.noreply.github.com>
2023-03-16 01:15:51 +0100

    searchbar design
    fixed some paddings on searchbar to match figma design. made it smaller

--- src/js/templates/admin/users/userTemplate.js
+++ src/js/templates/admin/users/userTemplate.js
@@ -1,5 +1,5 @@
 // Author: Stian Kornbakk & Jonas Hope
-// Team: FE-User
+// Team: FE-User & warrior gallop
 
 /**
  * Template for rendering user onto page.
@@ -13,13 +13,13 @@ export function userTemplate(userData) {
   userInfoContainer.classList.add('d-flex', 'col-10');
 
   const nameContainer = document.createElement('p');
-  nameContainer.classList.add('col-6', 'px-2', 'py-3', 'mb-0', 'overflow-hidden');
+  nameContainer.classList.add('col-6', 'px-2', 'py-2', 'mb-0', 'overflow-hidden', 'small');
   const name = userData.fullName;
   nameContainer.append(name);
   userInfoContainer.appendChild(nameContainer);
 
   const emailContainer = document.createElement('p');
-  emailContainer.classList.add('col-6', 'px-2', 'py-3', 'mb-0', 'overflow-hidden', 'small', 'fw-light');
+  emailContainer.classList.add('col-6', 'px-2', 'py-2', 'mb-0', 'overflow-hidden', 'small', 'fw-light');
   const email = userData.email;
   emailContainer.append(email);
   userInfoContainer.appendChild(emailContainer);
@@ -30,7 +30,7 @@ export function userTemplate(userData) {
   listingIconsContainer.classList.add('d-flex', 'w-100', 'justify-content-between');
 
   const activeContainer = document.createElement('div');
-  activeContainer.classList.add('mx-auto', 'd-none', 'd-lg-block', 'text-center', 'my-3', 'd-flex', 'align-self-center');
+  activeContainer.classList.add('mx-auto', 'd-none', 'd-lg-block', 'text-center', 'my-2', 'd-flex', 'align-self-center');
   const active = document.createElement('img');
   if (userData.isActive === true) {
     active.src = '/src/assets/icons/checkmark.svg';
@@ -44,7 +44,7 @@ export function userTemplate(userData) {
   listingIconsContainer.appendChild(activeContainer);
 
   const deleteProfileButton = document.createElement('button');
-  deleteProfileButton.classList.add('mx-auto', 'text-center', 'my-3', 'border-0', 'p-0', 'bg-transparent');
+  deleteProfileButton.classList.add('mx-auto', 'text-center', 'my-2', 'border-0', 'p-0', 'bg-transparent');
   const deleteProfileIcon = document.createElement('img');
   deleteProfileIcon.src = '/src/assets/icons/delete-black.svg';
   deleteProfileIcon.classList.add('deleteIcon');
