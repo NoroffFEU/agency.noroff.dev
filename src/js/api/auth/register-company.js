69891ee71057e3344e1f77132a07a1317f307297
Megumi Kimura <kimuramegumi@gmail.com>
2023-03-22 15:58:56 +0100

    Update register-company function

--- src/js/api/auth/register-company.js
+++ src/js/api/auth/register-company.js
@@ -1,29 +1,20 @@
 import { apiUrl } from '../constants.js';
 
-const action = '/auth/register';
+const action = 'auth/register';
 const method = 'post';
 
 export async function registerCompany(profile) {
   const registerURL = apiUrl + action;
 
-  try {
-    const response = await fetch(registerURL, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method,
-      body: JSON.stringify(profile),
-    });
+  const response = await fetch(registerURL, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method,
+    body: JSON.stringify(profile),
+  });
 
-    switch (response.status) {
-      case 201:
-        const result = await response.json();
-        window.location.replace('/pages/auth/login/index.html');
-        return result;
-      default:
-        throw new Error(`${response.status} ${response.statusText}`);
-    }
-  } catch (error) {
-    console.log(error);
+  if (!response.ok) {
+    throw new Error(response.statusText);
   }
 }
