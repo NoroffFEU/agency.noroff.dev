dac8b0dabdd739c7f743524b530deef3fb1a5dc2
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-03-23 19:47:08 +0100

    corrects url and  Updates function

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,12 +1,12 @@
 import { apiUrl } from '../constants.js';
 
-const action = '/auth/register';
+const action = 'auth/register';
 const method = 'post';
 
 export async function register(profile) {
+
   const registerURL = apiUrl + action;
 
-  try {
     const response = await fetch(registerURL, {
       headers: {
         'Content-Type': 'application/json',
@@ -15,15 +15,12 @@ export async function register(profile) {
       body: JSON.stringify(profile),
     });
 
-    switch (response.status) {
-      case 201:
-        const result = await response.json();
-        window.location.replace('/pages/auth/login/index.html');
-        return result;
-      default:
-        throw new Error(`${response.status} ${response.statusText}`);
+    if (!response.ok) {
+      throw new Error(response.statusText);
     }
-  } catch (error) {
-    console.log(error);
-  }
+
+    const result = await response.json();
+    return result;
 }
+
+
