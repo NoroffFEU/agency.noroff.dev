d4e223eb1f0ab134e498b67bb9af8be7a5187290
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-03-23 19:50:32 +0100

    Adds error message

--- src/js/listeners/auth/setRegisterFormListenerApplicant.js
+++ src/js/listeners/auth/setRegisterFormListenerApplicant.js
@@ -6,6 +6,7 @@ import { register } from '../../api/auth/index.js';
 export function setRegisterFormListenerApplicant(){
  
   const form = document.querySelector('#registerForm-applicant');
+  const userFeedback = document.querySelector('.user-feedback');
 
   if (form) {
     form.addEventListener('submit',async (event) => {
@@ -18,11 +19,13 @@ export function setRegisterFormListenerApplicant(){
     try{
        // send it to Api
       await register(data);
+      window.location.replace('/pages/auth/login/');
      }catch (error) {
-      // display error message here
-      console.log("error:", error);
+        // At the time of writing, API is out of order,
+        // But it should display different error messages according to the error received by API.
+      userFeedback.innerText = 'An error occurred. Please check the inputs.';
+      console.warn('error:', error);
     }
-  
     });
   }
 }
