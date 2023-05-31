96421aec25cb04742973945ead663f9cdb0c6549
DrRuski <ilia_e@hotmail.com>
2023-05-31 13:57:06 +0200

    Updated the test, still ain't working though.

--- src/js/utilities/search/searchFunctionTemplate.test.js
+++ src/js/utilities/search/searchFunctionTemplate.test.js
@@ -1,38 +1,58 @@
 import { searchFunction } from "./searchFunctionTemplate.js";
+import { getListOfListings } from "../../api/posts/getListOfListings.js";
+import { renderListings } from "../../templates/listings/renderListings.js";
+import { getSearchTermsListings } from "./getSearchTerms.js";
+import { listingNotFound } from "./searchNotFoundDisplay.js";
+
+jest.mock("../../api/posts/getListOfListings.js");
+jest.mock("../../templates/listings/renderListings.js");
+jest.mock("./getSearchTerms.js");
+jest.mock("./searchNotFoundDisplay.js");
 
 describe("searchFunction", () => {
-  let container;
   let searchInput;
+  let container;
 
   beforeEach(() => {
-    container = document.createElement("div");
-    container.classList.add("listingContainer");
     searchInput = document.createElement("input");
     searchInput.id = "searchListing";
+    container = document.createElement("div");
+    container.classList.add("listingContainer");
     document.body.appendChild(container);
     document.body.appendChild(searchInput);
   });
 
   afterEach(() => {
-    document.body.removeChild(container);
     document.body.removeChild(searchInput);
+    document.body.removeChild(container);
   });
 
-  it("should render listings when search term is valid", () => {
-    searchInput.value = "test";
+  it("should render listings when search input is empty", () => {
     searchFunction();
-    expect(container.innerHTML).not.toBe("");
+    expect(getListOfListings).toHaveBeenCalled();
+    expect(renderListings).toHaveBeenCalledWith(getListOfListings, container);
   });
 
-  it("should not render listings when search term is too short", () => {
-    searchInput.value = "te";
+  it("should render listings when search input is less than 3 characters", () => {
+    searchInput.value = "ab";
     searchFunction();
-    expect(container.innerHTML).toBe("");
+    expect(getListOfListings).toHaveBeenCalled();
+    expect(renderListings).toHaveBeenCalledWith(getListOfListings, container);
   });
 
-  it("should render search not found message when search term does not match any listings", () => {
-    searchInput.value = "notfound";
+  it("should render search results when search input is 3 or more characters", () => {
+    searchInput.value = "abc";
+    getSearchTermsListings.mockReturnValueOnce([{}]);
     searchFunction();
-    expect(container.innerHTML).toContain("Search not found");
+    expect(getSearchTermsListings).toHaveBeenCalledWith(getListOfListings, "abc");
+    expect(renderListings).toHaveBeenCalledWith([{}], container);
+  });
+
+  it("should display 'No results found' when search input matches no listings", () => {
+    searchInput.value = "def";
+    getSearchTermsListings.mockReturnValueOnce([]);
+    searchFunction();
+    expect(getSearchTermsListings).toHaveBeenCalledWith(getListOfListings, "def");
+    expect(listingNotFound).toHaveBeenCalledWith(container);
   });
 });
