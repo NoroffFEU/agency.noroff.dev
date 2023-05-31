84c108a93378b37f1e23c15160a6b03e536bce7f
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-05-31 19:24:29 +0200

    Adds success pop up to modal if form passes

--- src/js/listeners/post/createListing.js
+++ src/js/listeners/post/createListing.js
@@ -1,18 +1,19 @@
-import { create } from "../../api/posts/create.js";
+import { create } from '../../api/posts/create.js';
 
 /**
  * A listener for when users are creating a new listing
  */
 export async function createListing() {
-  const form = document.querySelector("#createNewListing");
-  const createTitle = document.querySelector("#createTitle");
-  const createLocation = document.querySelector("#createLocation");
-  const createDeadline = document.querySelector("#createDeadline");
-  const createPicture = document.querySelector("#pictureUrl");
-  const createDescription = document.querySelector("#createDescription");
+  const form = document.querySelector('#createNewListing');
+  const createTitle = document.querySelector('#createTitle');
+  const createLocation = document.querySelector('#createLocation');
+  const createDeadline = document.querySelector('#createDeadline');
+  const createPicture = document.querySelector('#pictureUrl');
+  const createDescription = document.querySelector('#createDescription');
+  const modalBody = document.querySelector('.modal-body');
 
   if (form) {
-    form.addEventListener("submit", async (event) => {
+    form.addEventListener('submit', async (event) => {
       event.preventDefault();
 
       const title = createTitle.value;
@@ -21,8 +22,6 @@ export async function createListing() {
       const picture = createPicture.value;
       const desc = createDescription.value;
       const currentTime = new Date().toISOString();
-
-
 
       if (createDeadline > currentTime) {
         await create({
@@ -36,10 +35,15 @@ export async function createListing() {
           img: picture,
           description: desc,
         });
+
         // pop up success message
         // redirect?
+        modalBody.innerHTML = `<img src="/src/assets/icons/checkmark.svg" alt="" />
+        <h3>Success</h3>`;
+
+        window.location.href = '../../../../pages/listings/listing/index.html';
       } else {
-        alert("Something went wrong");
+        alert('Something went wrong');
       }
     });
   }
