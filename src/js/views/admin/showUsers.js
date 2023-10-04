fbc1660b6041af52db32d3e24441b8a44896273e
ebergeng <59417456+ebergeng@users.noreply.github.com>
2023-10-04 16:26:27 +0200

    Made button ID unique and refactor code.

--- src/js/views/admin/showUsers.js
+++ src/js/views/admin/showUsers.js
@@ -1,59 +1,70 @@
-// Author: Linus Aakerberg
-// Team: FE-Offers
-
-const fetchUser = document.querySelector('#userProfile');
+// Author: Erlend Bergeng
+// Team: Elastic Meerkat
 
 // Import API url for fetch the data (dummy API for users data)
 import { dummyApiUrl } from '../../api/constants.js';
 // import headers for the requestOption
 import { headers } from '../../api/headers.js';
 // import deleteUser function
-import { deleteUser } from './deleteUsers.js';
-//import { deleteUserApi } from './deleteUsers.js';
 
-const userUrl = dummyApiUrl + `users`;
+/**
+ * Fetches and displays all users in the '#userProfile' container.
+ * 
+ * @async
+ * @function
+ * @returns {Promise<void>} Returns a promise that resolves once users are fetched and displayed.
+ * @throws {Error} Throws an error if there's an issue fetching or displaying the users.
+ */
+export async function showUsers() {
+  const container = document.querySelector('#userProfile');
+  const users = await getAllUsers()
+  let HTML = ``
+  users.forEach(user => {
+    const fullName = `${user.firstName} ${user.lastName}`
+    const email = user.email
+    const id = user.id
 
-export function showUsers() {
-  async function getUsers(url, data) {
-    try {
-      const reqOption = {
-        method: 'GET',
-        headers: headers(),
-        body: JSON.stringify(data),
-      };
-      const response = await fetch(userUrl, reqOption);
-      const json = await response.json();
+    HTML += `<tr>
+                    <th scope="row">${fullName}</th>
+                    <td>${email}</td>
+                    <td>
+                    <button class="btn btn-sm" id="deleteBtn${id}"><img src="/src/assets/icons/delete-black.svg" alt="Delete button" class="footerIcon" /></button>
+                    </td>
+                  </tr>`; 
+  });
+  container.innerHTML = HTML
 
-      const userData = json.users;
-
-      //  For loop to get all the users registered in the dataBase
-
-      for (let i = 0; i < userData.length; i++) {
-        /* API DUMMY - Fetch from user profiles and added company name + title */
-        // Cleaning up name from the API call.
-        // profileName = get and combine both firstName and lastName under same const
-        const profileName = userData[i].firstName + ` ` + userData[i].lastName;
-        // profileEmail = get email registered
-        const profileEmail = userData[i].email;
-        // profileId = get unique profile ID
-        const profileId = userData[i].id;
-
-        fetchUser.innerHTML += `
-        <tr>
-        <th scope="row">${profileName}</th>
-        <td>${profileEmail}</td>
-        <td>
-        <button class="btn btn-sm" id="deleteUserBtn"><img src="/src/assets/icons/delete-black.svg" alt="Delete button" class="footerIcon" /></button>
-        </td>
-      </tr>
-        `;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  getUsers();
+  /*
+  //-------- Commented out for now, addDeleteUserButtonEventListener is not ready. ----------//
+  users.forEach(user => {
+    const userId = user.id
+    addDeleteUserButtonEventListener(userId)
+  })
+  */
 }
 
-showUsers();
+/**
+ * Fetches all users from the dummy API.
+ * 
+ * @async
+ * @function
+ * @returns {Promise<Array<Object>|Error>} Returns a promise that resolves with an array of user objects, or rejects with an error.
+ * @throws {Error} Throws an error if there's an issue fetching the users.
+ */
+export async function getAllUsers() {
+  const userUrl = dummyApiUrl + `users`;
+
+  const options = {
+    method: "GET",
+    headers: headers()
+  }
+
+  try {
+    const response = await fetch(userUrl, options)
+    const json = await response.json()
+    return json.users
+  }catch(err) {
+    console.log(err)
+    return err
+  }
+}
