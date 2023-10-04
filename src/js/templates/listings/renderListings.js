ea02f9f89af71a3e2234e1d80c2c5daaeedbcff2
Eirik Berget Haukeland <eirik@haukeland.name>
2023-10-04 17:37:50 +0200

    limit the lines on the card-text, added bold to the title, working on the button in the bottom

--- src/js/templates/listings/renderListings.js
+++ src/js/templates/listings/renderListings.js
@@ -14,21 +14,24 @@ export async function renderListings() {
     listings.forEach(listing => {
         listingsContainer.innerHTML += 
        `
-       <div class="col-sm-6">
-         <div class="card my-3 px-3 bg-theme-light shadow">
+       <div class="card col-sm-6">
+         <div class="row my-3 px-3 bg-theme-light shadow">
            <div class="col-md-4 mt-5 px-2">
              <img src="../../images/logoipsum-287.svg" class="img-fluid rounded-start" alt="..." />
            </div>
            <div class="col-md-8">
              <div class="card-body">
-               <h5 class="card-title">Lorem ipsum dolor sit amet.</h5>
-               <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ducimus vitae ipsam tempore! Reiciendis soluta doloremque tempore harum, fugiat laudantium optio eveniet quae deleniti iste.</p>
+               <h5 class="card-title fw-bold">Lorem ipsum dolor sit amet.</h5>
+               <p 
+                 class="card-text overflow-hidden" 
+                 style="-webkit-line-clamp: 2; display: -webkit-box; -webkit-box-orient: vertical;"
+               >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ducimus vitae ipsam tempore! Reiciendis soluta doloremque tempore harum, fugiat laudantium optio eveniet quae deleniti iste.</p>
              </div>
-             <div class="position-absolute bottom-0 end-0 mx-3 my-3">
-               <div>
-                 <a href="#" class="btn bg-theme-primary">View More</a>
-               </div>
-           </div>
+             <div class="d-flex align-items-end mx-3 my-3" style="font-size: .75rem">
+               <span>(4) Applications</span>
+               <span>dd.mm.yy</span>
+               <a href="#" class="btn bg-theme-primary py-1">View</a>
+             </div>
            </div>
          </div>
        </div>
