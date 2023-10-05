49f05e02072317947f3b1661f569aead1ecb19c6
ebergeng <ebergeng@gmail.com>
2023-10-05 12:05:51 +0200

    moved function getAllUsers to getAllUsers.js

--- src/js/views/admin/showUsers.js
+++ src/js/views/admin/showUsers.js
@@ -1,11 +1,7 @@
 // Author: Erlend Bergeng
 // Team: Elastic Meerkat
 
-// Import API url for fetch the data (dummy API for users data)
-import { dummyApiUrl } from '../../api/constants.js';
-// import headers for the requestOption
-import { headers } from '../../api/headers.js';
-// import deleteUser function
+import { getAllUsers } from "../../api/users/getAllUsers.js"
 
 /**
  * Fetches and displays all users in the '#userProfile' container.
@@ -41,30 +37,4 @@ export async function showUsers() {
     addDeleteUserButtonEventListener(userId)
   })
   */
-}
-
-/**
- * Fetches all users from the dummy API.
- * 
- * @async
- * @function
- * @returns {Promise<Array<Object>|Error>} Returns a promise that resolves with an array of user objects, or rejects with an error.
- * @throws {Error} Throws an error if there's an issue fetching the users.
- */
-export async function getAllUsers() {
-  const userUrl = dummyApiUrl + `users`;
-
-  const options = {
-    method: "GET",
-    headers: headers()
-  }
-
-  try {
-    const response = await fetch(userUrl, options)
-    const json = await response.json()
-    return json.users
-  }catch(err) {
-    console.log(err)
-    return err
-  }
 }
