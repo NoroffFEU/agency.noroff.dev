e95aac3a346436a4a3550ea56ea072b95958b9a3
Eirik Berget Haukeland <eirik@haukeland.name>
2023-10-05 21:37:12 +0200

    added break points

--- src/js/templates/listings/renderListings.js
+++ src/js/templates/listings/renderListings.js
@@ -14,12 +14,12 @@ export async function renderListings() {
     listings.forEach((listing, number) => {
         listingsContainer.innerHTML += 
        `
-       <div class="col-sm-6">
+       <div class="col-12 col-lg-6">
          <div class="row p-2 px-3 g-3 bg-theme-light m-0">
-           <div class="m-0 px-0 col-md-3 d-flex flex-column justify-content-center">
+           <div class="m-0 px-0 col-3 d-flex flex-column justify-content-center">
              <img src="../../images/logoipsum-287.svg" class="img-fluid rounded-start" alt="..." />
            </div>
-           <div class="m-0 col-md-9 d-flex flex-column gap-2 align-items-baseline">
+           <div class="m-0 col-9 d-flex flex-column gap-2 align-items-baseline">
              <div class="card-body d-flex flex-column gap-2 w-100">
                <h5 class="card-title fw-bold text-truncate">Lorem ipsum dolor sit amet.</h5>
                <p 
@@ -27,7 +27,7 @@ export async function renderListings() {
                  style="-webkit-line-clamp: 2; display: -webkit-box; -webkit-box-orient: vertical;"
                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ducimus vitae ipsam tempore! Reiciendis soluta doloremque tempore harum, fugiat laudantium optio eveniet quae deleniti iste.</p>
              </div>
-             <div class="d-flex align-items-end justify-content-between w-100" style="font-size: .75rem">
+             <div class="d-flex flex-column flex-sm-row align-items-end justify-content-between w-100" style="font-size: .75rem">
                <span class="text-nowrap">(4) Applications</span>
                <span>dd.mm.yy</span>
                <a href="#" class="bg-theme-primary text-theme-light px-3 text-decoration-none" style="font-size: 1rem">View</a>
