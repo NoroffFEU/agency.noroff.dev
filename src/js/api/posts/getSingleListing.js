7cd4b8686dd7efd2c9502bd732c82fe5cf9be36c
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2023-10-13 14:09:56 +0200

    Add documentation to js functions
    Add JSdocs to js functions that don't have any documentation

--- src/js/api/posts/getSingleListing.js
+++ src/js/api/posts/getSingleListing.js
@@ -3,6 +3,13 @@
 import { apiBaseFetch } from '../apiBaseFetch.js';
 /*import { apiUrl } from '../constants.js';*/
 
+/**
+ * This functino sends a GET request to the API to retrieve the details of a single listing 
+ * 
+ * @param {string} id - The unique identifier of the listing to be retrieved. 
+ * @returns {Promise<Response>} 
+ */
+
 export async function getSingleListing(id) {
   if (!id) {
     throw new Error('Get requires a listingID');
--- src/js/api/users/editCompany.js
+++ src/js/api/users/editCompany.js
@@ -3,6 +3,14 @@ import { apiPath } from '../constants.js';
 const method = 'PUT';
 const action = '/user/';
 
+/**
+ * This function sends a PUT request to the API to update the company profile 
+ * 
+ * @param {Object} profile The updated company profile data
+ * @returns {Promise<Object>} A Promise that resolves with the updated company profile
+ * @throws {Error} If the 'id' is missing, the API request fails, or if it returns an error status 
+ */
+
 export async function editCompany(profile) {
   const { id } = profile;
   const profileURL = apiPath + action + `/${id}`;
