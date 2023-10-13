5211871bbe9c064aef65709f7de16506c87a2f39
saibia8 <saibia.design@gmail.com>
2023-10-13 21:43:50 +0200

    Revert "Get first,lastNames, fetch data and check for err"
    This reverts commit ffcad9cd39d545cab9d8f665566f73137e7b65e4.

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -4,45 +4,27 @@ const action = 'users';
 const method = 'POST';
 
 export async function register(profile) {
-  let data, error;
   const registerURL = apiUrl + action;
+
   try {
     const response = await fetch(registerURL, {
       headers: {
         'Content-Type': 'application/json',
       },
       method,
+      mode: 'cors',
       body: JSON.stringify(profile),
     });
-    const json = await response.json();
-    
-    if (response.ok) {
-      data = json;
-    } else {
-      error = extractError(json);
-    }
-    /* switch (response.status) {
+
+    switch (response.status) {
       case 201:
         const result = await response.json();
         window.location.replace('/pages/auth/login/index.html');
         return result;
       default:
         throw new Error(`${response.status} ${response.statusText}`);
-    } */
-
-  } catch (err) {
-    error = err.toString();
+    }
+  } catch (error) {
+    console.log(error);
   }
-  return { data, error };
-}
-
-function extractError(responseData) {
-  if (
-    responseData &&
-    responseData.errors &&
-    Array.isArray(responseData.errors)
-  ) {
-    return responseData.errors.map((error) => error.message).join("\n");
-  }
-  return "There was an error processing the request.";
 }
