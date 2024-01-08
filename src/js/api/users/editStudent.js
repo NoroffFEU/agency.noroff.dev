243e24b6f26cef7ed9ebb3a8a7140de85b6a2658
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2024-01-08 15:00:06 +0100

    Now gets the user ID and access token

--- src/js/api/users/editStudent.js
+++ src/js/api/users/editStudent.js
@@ -1,28 +1,38 @@
 import { apiPath } from '../constants.js';
+import { getToken } from '../getToken.js';
 
 const method = 'PUT';
 const action = 'users/';
 
 /**
- * This function sends a PUT request to the API to update the company profile 
- * 
+ * This function sends a PUT request to the API to update the company profile
+ *
  * @param {Object} profile The updated user profile data
  * @returns {Promise<Object>} A Promise that resolves with the updated user profile
- * @throws {Error} If the 'id' is missing, the API request fails, or if it returns an error status 
+ * @throws {Error} If the 'id' is missing, the API request fails, or if it returns an error status
  */
 
 export async function editStudent(profile) {
-  const { id } = profile;
-  const profileURL = apiPath + action + `${id}`;
+  // const { id } = profile;
+  const id = localStorage.getItem('id');
+  const newId = id.replace(/^"|"$/g, '');
+
+  const profileURL = apiPath + action + newId;
+
+  console.log('hi', profile);
+  const accessToken = getToken('token');
+  const newAccessToken = accessToken.replace(/^"|"$/g, '');
+  console.log('accessToken', newAccessToken);
+
   const body = JSON.stringify(profile);
   const options = {
     method,
     body,
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${newAccessToken}`,
     },
   };
-
   try {
     const response = await fetch(profileURL, options);
     const profile = await response.json();
