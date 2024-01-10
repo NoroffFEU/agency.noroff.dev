bde16ff1969d8f4628585f2ce45bc5dd640c9bb0
HMAsp <hanand55935@stud.noroff.no>
2024-01-10 09:58:19 +0100

    Update Vite config with build and server options

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,41 @@
+/* eslint-disable no-undef */
+import * as path from 'path';
+
+/** @type {import('vite').UserConfig} */
 export default {
-    base: '/',
-    server: {
-      port: 5173,
-      hot: true,
-      host: '127.0.0.1'
+  // base: '/',
+  build: {
+    outDir: path.resolve(__dirname, '/dist'),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'index.html'),
+        admin: path.resolve(__dirname, 'pages/admin/index.html'),
+        login: path.resolve(__dirname, 'pages/auth/login/index.html'),
+        register: path.resolve(__dirname, 'pages/auth/register/index.html'),
+        applicant: path.resolve(__dirname, 'pages/auth/register/applicant/index.html'),
+        company: path.resolve(__dirname, 'pages/auth/register/company/index.html'),
+        listings: path.resolve(__dirname, 'pages/listings/index.html'),
+        listing: path.resolve(__dirname, 'pages/listings/listing/index.html'),
+        create: path.resolve(__dirname, 'pages/listings/listing/create/index.html'),
+        edit: path.resolve(__dirname, 'pages/listings/listing/edit/index.html'),
+        user: path.resolve(__dirname, 'pages/user/index.html'),
+        //
+        // Not in use yet
+        // tbd: path.resolve(__dirname, 'pages/TBD/index.html'),
+        // apply: path.resolve(__dirname, 'pages/TBD/apply/index.html'),
+        // client: path.resolve(__dirname, 'pages/TBD/client/application/index.html'),
+        // offer: path.resolve(__dirname, 'pages/TBD/offer-views/index.html'),
+      },
     },
-  };
+  },
+  server: {
+    port: 5173,
+    hot: true,
+    host: '127.0.0.1',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+};
