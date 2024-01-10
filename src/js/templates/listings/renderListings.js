28132a61694aa97f707cc1754f839d81974ad6f0
Karen Bonilla <114474341+KarenBonillaM@users.noreply.github.com>
2024-01-10 11:08:23 +0100

    Revert "#240 listings page style from wireframe"

--- src/js/templates/listings/renderListings.js
+++ src/js/templates/listings/renderListings.js
@@ -17,25 +17,23 @@ export async function renderListings() {
 
     data.forEach(function(data) {
       listingsContainer.innerHTML += 
-       `
-       <div class="col-12 col-lg-6">
-         <div class="row g-3 bg-theme-light m-0 shadow card card-listing rounded-0 bg-white">
-           <div class="m-0 col-3 d-flex flex-column justify-content-center img-fluid img-thumbnail card-img-top border-0 py-4 px-5" id="img-thumbnail">
-             <img src=${data.company.logo} class="rounded-start " alt="..." />
-           </div>
-           <div class="m-0 col-12 border-top d-flex flex-column gap-2 align-items-baseline py-1 px-4" id="listing-card-body">
-             <div class="card-body d-flex flex-column gap-2 w-100 pt-1 p-0">
-               <h5 class="card-title fw-bold text-truncate mb-0">${data.title}</h5>
-               <p 
-                 class="card-text overflow-hidden" 
-                 style="-webkit-line-clamp: 2; display: -webkit-box; -webkit-box-orient: vertical;"
-               >${data.description}</p>
-             </div>
-             <div class="d-flex flex-column flex-sm-row align-items-end justify-content-between w-100 pb-1" style="font-size: .75rem">
-               <span class="text-nowrap">Deadline</span>
-               <span>${date2}</span>
-               <a href="#" class="bg-theme-primary text-white px-3 text-decoration-none" style="font-size: 1rem">View</a>
-             </div>
+     `
+     <div class="col-12 col-lg-6">
+       <div class="row p-2 px-3 g-3 bg-theme-light m-0 shadow">
+         <div class="m-0 p-3 col-3 d-flex flex-column justify-content-center">
+           <img src=${data.company.logo} class="img-fluid rounded-start" alt="..." />
+         </div>
+         <div class="m-0 col-9 d-flex flex-column gap-2 align-items-baseline">
+           <div class="card-body d-flex flex-column gap-2 w-100">
+             <h5 class="card-title fw-bold text-truncate">${data.title}</h5>
+             <p 
+               class="card-text overflow-hidden" 
+               style="-webkit-line-clamp: 2; display: -webkit-box; -webkit-box-orient: vertical;"
+             >${data.description}</div>
+           <div class="d-flex flex-column flex-sm-row align-items-end justify-content-between w-100" style="font-size: .75rem">
+             <p>Deadline</></br>
+             <span>${date2}</span>
+             <a href="#" class="bg-theme-primary text-theme-black px-3 text-decoration-none" style="font-size: 1rem">View</a
            </div>
          </div>
        </div>
