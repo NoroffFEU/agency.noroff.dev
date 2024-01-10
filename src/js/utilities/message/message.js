22eef62c412552eefbe08bb392f0db192fad24b1
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-01-10 20:39:18 +0100

    Adds JSdocs to message function

--- src/js/utilities/message/message.js
+++ src/js/utilities/message/message.js
@@ -1,3 +1,10 @@
+/**
+ * Dynamic function that displays a message to the user by pre-defined message type, content and a target element.
+ *
+ * @param {string} type - The type of message, i.e. "danger", "info", "warning" etc.
+ * @param {string} content - The content of the message.
+ * @param {string} target - Target element class or ID.
+ */
 export function message(type, content, target) {
   const element = document.querySelector(target);
   element.innerHTML = '';
