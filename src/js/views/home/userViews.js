5392d43aaf74ba2a370a5bc6846b7727becfbb79
maleneivy <68667843+maleneivy@users.noreply.github.com>
2024-01-11 11:16:41 +0100

    Added title for homepage and a button onclick href to update profile btn

--- src/js/views/home/userViews.js
+++ src/js/views/home/userViews.js
@@ -15,7 +15,7 @@ https://i.postimg.cc/8Pb4Lrkf/homepage-applicant-update-profile.jpg
 <p><span class="fw-semibold">Find talents for your organisation.</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 <div class="buttonContainer mx-auto mx-md-0">
-<button class="btn customButton btn-theme-primary text-theme-light fw-semibold w-100">Update Profile</button>
+<button class="btn customButton btn-theme-primary text-theme-light fw-semibold w-100" onclick="window.location.href='../../../../pages/user/index.html'">Update Profile</button>
 </div>
 </div>
 </div>
