6b345d2ec08bf061828c01cbcd3d4de2005c71df
Tokle89 <fredriktokle@gmail.com>
2024-01-11 17:33:50 +0100

    adds the lost code from ticket 494, for fetching and rendering listings

--- src/js/api/posts/getListOfListings.js
+++ src/js/api/posts/getListOfListings.js
@@ -1,27 +1,22 @@
 // Author: Emilie Herrera Thomsen
 // The Dummy json needs to be replaced with actual endpoint.
 
+//Updated by : Fredrik Tokle
+// Removed  the getListOfListings() from the file, as it served no purpose.
+//updated the url to the actual API endpoint.
 
-import { authBaseFetchOpen} from "../apiBaseFetch.js";
-import { apiPath } from "../constants.js";
-import { headers } from "../headers.js";
-
-
-
-
+import { authBaseFetchOpen } from '../apiBaseFetch.js';
+import { apiPath } from '../constants.js';
 
 export async function getListOfListings() {
-    const getListingsUrl = apiPath;
+  const baseUrl = apiPath;
+  const listingUrl = baseUrl + `listings`;
 
-  const data = await fetch(getListingsUrl, {
-      headers: headers(),
-      body: JSON.stringify(),
-    }
-    );
-     if (response.ok) {
-   return await data.json();
-  } else {
+  const response = await authBaseFetchOpen(listingUrl);
+
+  if (!response.ok) {
     console.error(`Error: ${response.status} ${response.statusText}`);
+  } else {
+    return response.json();
   }
-    
-  }
+}
