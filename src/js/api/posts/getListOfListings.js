f4ad48ae8a1aaaf95a182c3195219988674906e9
Ernesto Jr Fundal Osorio <114483893+nyolarraklay@users.noreply.github.com>
2024-01-11 11:53:53 +0000

    change code for datahandling in the listingPage

--- src/js/api/posts/getListOfListings.js
+++ src/js/api/posts/getListOfListings.js
@@ -1,27 +1,21 @@
 // Author: Emilie Herrera Thomsen
 // The Dummy json needs to be replaced with actual endpoint.
 
-
-import { authBaseFetchOpen} from "../apiBaseFetch.js";
-import { apiPath } from "../constants.js";
-import { headers } from "../headers.js";
-
-
-
-
+import { apiPath } from '../constants.js';
+import { headers } from '../headers.js';
 
 export async function getListOfListings() {
-    const getListingsUrl = apiPath;
-
+  const getListingsUrl = apiPath;
   const data = await fetch(getListingsUrl, {
-      headers: headers(),
-      body: JSON.stringify(),
-    }
-    );
-     if (response.ok) {
-   return await data.json();
+    method: 'GET',
+    headers: headers(),
+    body: JSON.stringify(),
+  });
+
+  const dataListings = await data.json();
+  if (data.ok) {
+    return dataListings;
   } else {
-    console.error(`Error: ${response.status} ${response.statusText}`);
+    console.error(`Error: ${data.status} ${data.statusText}`);
   }
-    
-  }
+}
