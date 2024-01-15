b956c93fdce38c205d92dd03704e122662e8ff40
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2024-01-15 19:57:42 +0100

    Edited feature for editing listings
    Contains lots of comments that must be removed before merging

--- src/js/listeners/post/editListings.js
+++ src/js/listeners/post/editListings.js
@@ -2,21 +2,61 @@
 
 import { editSingleListing } from '../../api/posts/editSingleListing.js';
 
-export async function editListingListener() {
-  const form = document.querySelector('#editListings');
+export function editListingListener() {
+  const form = document.querySelector('#editListing');
+  form.addEventListener('submit', editListingListenerForm);
+}
+
+async function editListingListenerForm() {
+  event.preventDefault();
 
   const url = new URL(location.href);
   const id = url.searchParams.get('id');
 
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const formData = new FormData(form);
-    const listing = Object.fromEntries(formData.entries());
-    listing.id = id;
+  const form = event.target;
+  const formData = new FormData(form);
+  const listing = Object.fromEntries(formData.entries());
 
-    //send it to API
-    editSingleListing(listing);
-    location.reload();
-  });
+  //send it to API
+  editSingleListing(id, listing);
 }
+
+// const form = document.querySelector('#editListing');
+// form.addEventListener('submit', editListingListener);
+
+// export async function editListingListener() {
+//   event.preventDefault();
+
+//   const url = new URL(location.href);
+//   const id = url.searchParams.get('id');
+
+//   const form = event.target;
+//   const formData = new FormData(form);
+//   const listing = Object.fromEntries(formData.entries());
+
+//   //send it to API
+//   editSingleListing(id, listing);
+// }
+
+// import { editSingleListing } from '../../api/posts/editSingleListing.js';
+
+// console.log('Hello12322222');
+// export async function editListingListener() {
+//   event.preventDefault();
+//   const form = document.querySelector('#editListing');
+
+//   const url = new URL(location.href);
+//   const id = url.searchParams.get('id');
+
+//   form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     const form = event.target;
+//     const formData = new FormData(form);
+//     const listing = Object.fromEntries(formData.entries());
+//     listing.id = id;
+
+//     //send it to API
+//     editSingleListing(listing);
+//     // location.reload();
+//   });
+// }
