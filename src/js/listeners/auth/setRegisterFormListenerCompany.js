077839563c26a79a3742efbc66dd75ce3b241216
Kim Rune Møller <kimrune@hotmail.no>
2024-01-17 09:28:58 +0100

    extending functionality to create both user and company

--- src/js/listeners/auth/setRegisterFormListenerCompany.js
+++ src/js/listeners/auth/setRegisterFormListenerCompany.js
@@ -1,6 +1,4 @@
-// Register form for company
-
-import { registerCompany } from '../../api/auth/index.js';
+import { registerUser, registerCompany } from '../../api/auth/index.js';
 
 export function setRegisterFormListenerCompany() {
   const form = document.querySelector('#registerForm-company');
@@ -8,19 +6,41 @@ export function setRegisterFormListenerCompany() {
   if (form) {
     form.addEventListener('submit', async (event) => {
       event.preventDefault();
-      const form = event.target;
-      const formData = new FormData(form);
-      const profile = Object.fromEntries(formData.entries());
-      const imageUrl = document.querySelector('#imageUrl').value;
-      const data = { ...profile, imageUrl };
+      
+      const formData = new FormData(event.target);      
+      const formEntries = Object.fromEntries(formData.entries());
+      const userNameArray = formEntries.name.split(' ');
 
+      const userData = {
+        firstName: userNameArray[0],
+        lastName: userNameArray.slice(1).join(' '),
+        email: formEntries.email,
+        password: formEntries.password,
+        role: 'Client',
+      };
+      
       try {
-        // send it to Api
-        await registerCompany(data);
-      } catch (error) {
-        // display error message here
+        const userResult = await registerUser(userData);    
+        const userProfile = userResult.data;
+        
+        const companyData = {
+          name: formEntries.name,                
+          email: formEntries.email,
+          sector: formEntries.sector,
+          website: formEntries.website,  
+          phone: formEntries.phone,    
+          logo: "", // TODO: Add logo
+          admin: userProfile.id,        
+          registerToken: userProfile.token,
+        };
+
+        const companyResult = await registerCompany(companyData);
+        console.log(companyResult);
+
+      } catch (error) {        
         console.log('error:', error);
-      }
+      } 
+      
     });
   }
 }
