a0b923b68765e2f162f17200a022f32b0d06ed56
emilrh91 <emilhalvorsen91@gmail.com>
2024-01-18 21:01:56 +0100

    made it load the about me field

--- src/js/listeners/profile/handleModalData.js
+++ src/js/listeners/profile/handleModalData.js
@@ -1,12 +1,14 @@
 import { showUserDatails } from './showUserDetails.js';
 
 export function handleModalData() {
+  const aboutField = document.querySelector('#studentDescription');
   const skillsField = document.querySelector('#studentSkills');
   const editProfileModal = document.getElementById('editProfileModal');
 
   editProfileModal.addEventListener('show.bs.modal', async function () {
     const profileData = await showUserDatails();
 
+    aboutField.value = profileData.about;
     skillsField.value = profileData.skills.join(', ');
   });
 }
