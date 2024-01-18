59c13aa59e6effefbe1e533632ff6cfb5f69b49e
Kim Rune Møller <kimrune@hotmail.no>
2024-01-18 13:19:45 +0100

    adding validation

--- src/js/listeners/auth/setRegisterFormListenerCompany.js
+++ src/js/listeners/auth/setRegisterFormListenerCompany.js
@@ -1,23 +1,72 @@
 import { registerUser, registerCompany } from '../../api/auth/index.js';
 import getCompanies from '../../api/company/companies.js';
+import { message } from '../../utilities/message/message.js';
+import { inputs } from './validateInputs.js';
 
 export function setRegisterFormListenerCompany() {
   const form = document.querySelector('#registerForm-company');
 
   if (form) {
-    form.addEventListener('submit', async (event) => {        
+    const email = form.querySelector('#userEmail');
+    const companyName = form.querySelector('#companyName');
+    const password = form.querySelector('#userPassword');
+    const repeatPassword = form.querySelector('#userConfirmPassword');
+    const sector = form.querySelector('#companySector');
+    const user = form.querySelector('#userName');
 
+    email.addEventListener('blur', () => {
+      inputs.validateCompanyEmail(email, false);
+    });
+    companyName.addEventListener('blur', (event) => {
+      inputs.validateCompanyName(event.target, false);
+    });
+    password.addEventListener('blur', () => {
+      inputs.validatePassword(password, false);
+    });
+    repeatPassword.addEventListener('blur', () => {
+      inputs.validateRepeatPassword(password, repeatPassword, false);
+    });
+    sector.addEventListener('blur', () => {
+      inputs.validateSector(sector, false);
+    });
+    user.addEventListener('blur', () => {
+      inputs.validateFullName(user, false);
+    });
+
+    form.addEventListener('submit', async (event) => {
       event.preventDefault();
 
       const successElement = document.querySelector('#registrationSuccessMessage');
-      const errorElement = document.querySelector('#registrationErrorMessage');
-      
-      const formData = new FormData(event.target);      
-      const formEntries = Object.fromEntries(formData.entries());      
+      const errorElement = document.querySelector('#errorMessage');
+
+      const isEmailValid = inputs.validateCompanyEmail(email);
+      const isCompanyNameValid = inputs.validateCompanyName(companyName);
+      const isPasswordValid = inputs.validatePassword(password);
+      const isRepeatPasswordValid = inputs.validateRepeatPassword(password, repeatPassword);
+      const isSectorValid = inputs.validateSector(sector);
+      const isFullNameValid = inputs.validateFullName(user);
+
+      if (
+        !isEmailValid ||
+        !isCompanyNameValid ||
+        !isPasswordValid ||
+        !isRepeatPasswordValid ||
+        !isSectorValid ||
+        !isFullNameValid
+      ) {
+        message('danger', 'Invalid registration credentials. Please try again', '#errorMessage');
+        return;
+      }
+
+      const formData = new FormData(event.target);
+      const formEntries = Object.fromEntries(formData.entries());
 
       const companyNameExists = await checkCompanyNameExists(formEntries.name);
       if (companyNameExists) {
-        showMessage(errorElement, `Company name <strong>${formEntries.name}</strong> already exists`);        
+        showMessage(
+          errorElement,
+          `Company name <strong>${formEntries.name}</strong> already exists`
+        );
         return;
       }
 
@@ -28,10 +77,10 @@ export function setRegisterFormListenerCompany() {
         email: formEntries.email,
         password: formEntries.password,
         role: 'Client',
-      };      
+      };
 
-      try {               
-        const userResult = await registerUser(userPayload);    
+      try {
+        const userResult = await registerUser(userPayload);
         const userProfile = userResult.data;
 
         if (userResult.error) {
@@ -39,44 +88,46 @@ export function setRegisterFormListenerCompany() {
         }
 
         const companyPayload = {
-          name: formEntries.name,                
+          name: formEntries.name,
           email: formEntries.email,
           sector: formEntries.sector,
-          website: formEntries.website,  
-          phone: formEntries.phone,    
-          logo: "", // TODO: Add logo
-          admin: userProfile.id,        
+          website: formEntries.website,
+          phone: formEntries.phone,
+          logo: '', // TODO: Add logo
+          admin: userProfile.id,
           registerToken: userProfile.token,
         };
 
         const companyResult = await registerCompany(companyPayload);
         if (companyResult.error) {
           throw new Error(companyResult.error);
-        }         
+        }
 
-      removeMessage(errorElement);
-      showMessage(successElement, `Company <strong>${formEntries.name}</strong> registered successfully! <a href="/pages/auth/login/index.html">Login</a> to continue.`);
-
-      } catch (error) {        
-        showMessage(errorElement, `Error: ${error.message}`);        
-      }       
+        removeMessage(errorElement);
+        showMessage(
+          successElement,
+          `Company <strong>${formEntries.name}</strong> registered successfully! <a href="/pages/auth/login/index.html">Login</a> to continue.`
+        );
+      } catch (error) {
+        showMessage(errorElement, `Error: ${error.message}`);
+      }
     });
   }
 }
 
 async function checkCompanyNameExists(name) {
   const companyList = await getCompanies();
-  return companyList.some(company => company.name === name);  
+  return companyList.some((company) => company.name === name);
 }
 
-function showMessage(element,message) {  
+function showMessage(element, message) {
   element.classList.remove('d-none');
   element.innerHTML = message;
 }
 
-function removeMessage(element) {  
+function removeMessage(element) {
   if (!element.classList.contains('d-none')) {
     element.classList.add('d-none');
-  }  
+  }
   element.innerHTML = '';
 }
