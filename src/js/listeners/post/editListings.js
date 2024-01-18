b0cfd4bba3f54f1322a23260972dbf134505e079
mathiasg12 <mathiasgausl@gmail.com>
2024-01-18 14:23:45 +0100

    added some JSdocs to functions in the src/js/listeners folder where they were missing

--- src/js/listeners/post/editListings.js
+++ src/js/listeners/post/editListings.js
@@ -1,7 +1,10 @@
 // Author: Gonzalo Longe
 
 import { editSingleListing } from '../../api/posts/editSingleListing.js';
-
+/**
+ * function that allows users to edit the a listing, if a user clicks the submit button a new object is created with the new input the function the sends the new object to the api
+ * after the listing is updated the page reloads
+ */
 export async function editListingListener() {
   const form = document.querySelector('#editListings');
 
