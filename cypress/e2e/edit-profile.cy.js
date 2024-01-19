d7968ceed8e2e49835ca7b52b08180d311c4d7b1
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-01-19 21:37:17 +0100

    Modifies edit profile details test

--- cypress/e2e/edit-profile.cy.js
+++ cypress/e2e/edit-profile.cy.js
@@ -11,6 +11,7 @@ describe('Edits profile details', () => {
     cy.visit('/'); // Please replace this with your local vite url
 
     // Login process
+    cy.contains('Log in').click();
     cy.get('#email').type('cypress-test@noroff.no');
     cy.get('#password').type('czv4euj*ncv6NUG@aqy');
     cy.get('button[type="submit"]').click();
@@ -20,30 +21,49 @@ describe('Edits profile details', () => {
     cy.contains('EDIT PROFILE').click();
 
     // Fill out the edit profile form
-    cy.get('#firstNameStudent').should('be.visible');
+    cy.wait(200);
+    cy.get('#editProfileModal').should('be.visible');
     cy.get('#firstNameStudent').clear();
     cy.get('#firstNameStudent').type('This', { delay: 100 });
     cy.get('#lastNameStudent').clear();
     cy.get('#lastNameStudent').type('User', { delay: 100 });
     cy.get('#studentSkills').clear();
-    cy.get('#studentSkills').type('E2E-testing, Cypress, JavaScript');
+    cy.get('#studentSkills').type('E2E-testing');
     cy.get('#studentDescription').clear();
-    cy.get('#studentDescription').type(
-      `Hello! This "About Me" section is written with a Cypress E2E test`
-    );
+    cy.get('#studentDescription').type(`First student description edit`);
 
     // Submit the form
-    cy.get('.d-grid > .fw-semibold').click();
+    cy.get('button').contains('SAVE').click();
 
     // Verify updated profile details
-    cy.visit('http://127.0.0.1:4173/pages/user/'); // Please replace the first part with your local vite url
+    cy.reload();
     cy.get('#profileName').should('contain', 'This User');
-    cy.get('#skillsList > :nth-child(1)').should('contain', 'E2E-testing');
-    cy.get('#skillsList > :nth-child(2)').should('contain', 'Cypress');
-    cy.get('#skillsList > :nth-child(3)').should('contain', 'JavaScript');
-    cy.get('#descriptionBody').should(
-      'contain',
-      `Hello! This "About Me" section is written with a Cypress E2E test`
-    );
+    cy.get('#skillsList').contains('E2E-testing');
+    cy.get('#descriptionBody').should('contain', `First student description edit`);
+
+    // Navigate to profile edit modal again
+    cy.contains('Profile').click();
+    cy.contains('EDIT PROFILE').click();
+
+    // Fill out the edit profile form again to assure changes happen
+    cy.wait(200);
+    cy.get('#editProfileModal').should('be.visible');
+    cy.get('#firstNameStudent').clear();
+    cy.get('#firstNameStudent').type('Test', { delay: 100 });
+    cy.get('#lastNameStudent').clear();
+    cy.get('#lastNameStudent').type('Student', { delay: 100 });
+    cy.get('#studentSkills').clear();
+    cy.get('#studentSkills').type('Cypress');
+    cy.get('#studentDescription').clear();
+    cy.get('#studentDescription').type(`New description for student`);
+
+    // Submit the form again
+    cy.get('button').contains('SAVE').click();
+
+    // Verify the new, updated profile details
+    cy.reload();
+    cy.get('#profileName').should('contain', 'Test Student');
+    cy.get('#skillsList').contains('Cypress');
+    cy.get('#descriptionBody').should('contain', `New description for student`);
   });
 });
