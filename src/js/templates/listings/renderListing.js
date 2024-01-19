5b9539260c787983c30be5048e7b055bf8f25187
runeunhjem <runeunhjem@gmail.com>
2024-01-19 13:45:17 +0100

    Changes logo size on single listing page

--- src/js/templates/listings/renderListing.js
+++ src/js/templates/listings/renderListing.js
@@ -6,24 +6,24 @@ import { findDaysAgo } from '../../utilities/dateConverter/dateConverter.js';
 export const renderListing = async () => {
   const url = new URL(location.href);
   const id = url.searchParams.get('id');
-  
+
     const metaDescription = document.head.children[4]
 
     const container = document.getElementById('listingContainer');
-  
+
     if (id) {
       const result = await getSingleListing(id);
       const {title, company } = result
       metaDescription.content = `View ${title} Application at Noroff Job Agency`
       const listing = createListing(result);
       container.append(listing);
-      
+
       //Have used userName and UserRole because i could not get the Store to work
       /* This could should call user name and role as profile from the class Store that handles local storage*/
       const userName = localStorage.getItem('email');
       const userRole = localStorage.getItem('role');
       const btnContainer = listing.querySelector('.buttonContainer');
-      
+
      /*The if statements should use profile username and role, but is using userName and userRole instead because of the issue written above */
         if (userName === company.name) {
           btnContainer.innerHTML = '';
@@ -31,18 +31,18 @@ export const renderListing = async () => {
           const deleteBtn = createElement("button", ["btn", "btn-theme-dark"], null, "Delete listing")
           btnContainer.append(editBtn, deleteBtn) ;
         } else if (userRole === 'Company') {
-      
+
           btnContainer.classList.add('d-none');
         }
- 
-    
-    
+
+
+
     } else {
       alert('No id provided');
       setTimeout(() => {
         location.href = '../index.html';
       }, 2000);
-      
+
     }
 }
 
@@ -57,12 +57,10 @@ element.append(card)
 return element
 }
 const  createImg = ({name, logo}) => {
-  const element = createElement("img", ["card-img-top", "rounded-0", "w-100"],null,null,null,logo, name)
-  element.style = "max-height: 250px; object-fit: cover; "
+  const element = createElement("img", ["listing-logo card-img-top", "rounded-0", "w-100"],null,null,null,logo, name)
   return element;
- 
-
 }
+
 const createCardBody = (title, description, deadline, company, created, requirements, tags) => {
   const element = createElement("div", ["card-body", "d-flex", "flex-column", "gap-2" ])
   const h1 = createElement("h1", ["card-title"], null, title)
