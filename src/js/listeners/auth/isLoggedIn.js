d59498fdcd2cf933164b38a5a6577d9da398f758
MariuszRozycki <mariansc83@icloud.com>
2024-02-27 22:03:51 +0100

    Change code inside isLoggedIn.js. Now after clicking in btn 'View listings', user goes to listings website.

--- src/js/listeners/auth/isLoggedIn.js
+++ src/js/listeners/auth/isLoggedIn.js
@@ -1,5 +1,4 @@
 import { Store } from '../../storage/storage.js';
-
 const tokenStore = new Store('token');
 /**
  * function that checks if the value of tokenStore.state is truthy or falsy, the function returns true if the user is logged in and the tokenStore.state is truthy and,
@@ -13,15 +12,19 @@ export function isUserLoggedIn() {
  * function that redirects the user when they press the viewListingsButton it redirects the user eighter to the listing specific page
  * if the user is logged in or to the login page if they are not
  */
+
+// Mariusz Rozycki comment - Hi Rikke. I just removed not necessary if... else block of code.
+// Function setInterval inside addEvent checks each 50 milliseconds is DOM element 'viewListingsButton' available on website. If it is, then it stops interval.
+// If viewListeningsButton exists than user can 'click' on this element and by using "window.location.href = '/pages/listings/index.html';"
+// user is moving to listings
 const addEvent = setInterval(() => {
   const viewListingsButton = document.getElementById('viewListingsButton');
 
-  viewListingsButton?.addEventListener('click', function () {
-    if (isUserLoggedIn()) {
+  if (viewListingsButton) {
+    viewListingsButton.addEventListener('click', function () {
       window.location.href = '/pages/listings/index.html';
-    } else {
-      window.location.href = '/pages/auth/login/index.html';
-    }
+    });
+
     clearInterval(addEvent);
-  });
+  }
 }, 50);
