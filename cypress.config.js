b8cc8f1f2bc34ee4411fcb44d61c2b13e4ac17a2
MariuszRozycki <mariansc83@icloud.com>
2024-03-17 00:24:58 +0100

    Change baseUrl in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     viewportHeight: 1080,
     // Mariusz Rozycki as QA-phoenix
     // I changed baseUrl from 'http://127.0.0.1:4173/' to 'http://127.0.0.1:5173/ (according readme.md in project)
-    baseUrl: 'http://127.0.0.1:5173/.', // 'http://127.0.0.1:4173/', // this is the Vite build server, not the dev server
+    baseUrl: 'http://127.0.0.1:5173/', // 'http://127.0.0.1:4173/', // this is the Vite build server, not the dev server
     video: false,
     setupNodeEvents(on, config) {
       config.env = {
