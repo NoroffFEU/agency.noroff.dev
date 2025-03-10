953a7c2c688608f90cee3ce2dbd1d0d5987f4bf7
Skr3d3 <skr3d3@gmail.com>
2025-03-10 20:10:04 +0100

    add simple redirect of unregistered users when accessing /pages/user/ in the url.

--- src/js/listeners/profile/profileRouter.js
+++ src/js/listeners/profile/profileRouter.js
@@ -4,16 +4,23 @@ import { handleEditForms } from './handleEditForm.js';
 import { modalToggle } from './modalToggle.js';
 import { showUserDetails } from './showUserDetails.js';
 import { handleModalData } from './handleModalData.js';
-import { toggleShowFavListings} from './toggleShowFavListings.js';
+import { toggleShowFavListings } from './toggleShowFavListings.js';
 //import { showUserListings } from './showUserListings.js';
 
 export function profileRouter() {
-  addDegreeInput();
-  handleAvatarInput();
-  modalToggle();
-  handleEditForms();
-  showUserDetails();
-  handleModalData();
-  toggleShowFavListings();
-  //showUserListings();
+  // Redirects unregister user when accessing /pages/user/
+  const role = localStorage.getItem('role')?.replace(/^"|"$/g, '').trim().toLowerCase();
+  console.log('role', role);
+  if (role === undefined) {
+    window.location.href = '../auth/register/applicant/';
+  } else {
+    addDegreeInput();
+    handleAvatarInput();
+    modalToggle();
+    handleEditForms();
+    showUserDetails();
+    handleModalData();
+    toggleShowFavListings();
+    //showUserListings();
+  }
 }
